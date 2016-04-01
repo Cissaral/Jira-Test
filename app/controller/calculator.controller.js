a0595dede14c6644fe61a118f87f84c67baeaa62
ser1971 <eichler.st@gmail.com>
2016-04-01 15:58:36 +0200

    Code erstellt für 1.Testfall 'Prüfe auf valide Eingabe'

--- app/controller/calculator.controller.js
+++ app/controller/calculator.controller.js
@@ -6,5 +6,22 @@ angular
 	.controller('CalculatorCtrl', CalculatorCtrl);
 
 function CalculatorCtrl(){
+	var vm = this;
+	// Controller-Variable
+	vm.eingabe1 = '';
+	vm.eingabe2 = '';
+
+	// controller API
+	vm.isNumber = isNumber;
+
+	function isNumber (wert){
+		if (!isNaN(wert) || wert === ''){
+			return true;
+			}
+			else {
+				return false;
+			}
+	}
 }
+
 })();
