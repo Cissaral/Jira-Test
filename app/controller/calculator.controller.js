f4876d5ce28edaf137d50a2df36cb9724953548d
ser1971 <eichler.st@gmail.com>
2016-04-11 20:33:00 +0200

    Berechnungsfunktion hinterlegt

--- app/controller/calculator.controller.js
+++ app/controller/calculator.controller.js
@@ -10,9 +10,11 @@ function CalculatorCtrl(){
 	// Controller-Variable
 	vm.eingabe1 = '';
 	vm.eingabe2 = '';
+	vm.ergebnis;
 
 	// controller API
 	vm.isNumber = isNumber;
+	vm.calculation = calculation;
 
 	function isNumber (wert){
 		if (!isNaN(wert) || wert === ''){
@@ -21,6 +23,23 @@ function CalculatorCtrl(){
 			else {
 				return false;
 			}
+	}
+
+	function calculation (eingabe1,operations,eingabe2){
+	switch(operations){
+		case '+' : 
+		vm.ergebnis = eingabe1 + eingabe2;
+		break;
+		case '-' : 
+		vm.ergebnis = eingabe1 - eingabe2;
+		break;
+		case '*' : 
+		vm.ergebnis = eingabe1 * eingabe2;
+		break;
+		case '/' : 
+		vm.ergebnis = eingabe1 / eingabe2;
+		break;
+	}
 	}
 }
 
